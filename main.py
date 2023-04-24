c9474670e32852cc0976310667c772e5584052b5
holmjohn <holmjohn@ourlol.ca>
2023-04-24 12:31:21 -0400

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,51 @@
+def check_winner():
+    if choice == opponent_choice:
+        basic.show_string("Tie")
+    elif choice == "Rock":
+        if opponent_choice == "Paper":
+            basic.show_leds("""
+                # . . . .
+                                # . . . .
+                                # . . . .
+                                # . . . .
+                                # # # # .
+            """)
+    else:
+        pass
+
+def on_button_pressed_a():
+    global choice
+    basic.show_icon(IconNames.SQUARE)
+    choice = "Rock"
+input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_ab():
+    global choice
+    basic.show_icon(IconNames.NO)
+    choice = "Scissors"
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_received_string(receivedString):
+    global opponent_choice
+    opponent_choice = receivedString
+radio.on_received_string(on_received_string)
+
+def on_button_pressed_b():
+    global choice
+    basic.show_icon(IconNames.CHESSBOARD)
+    choice = "Paper"
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+def on_gesture_shake():
+    radio.send_string(choice)
+input.on_gesture(Gesture.SHAKE, on_gesture_shake)
+
+opponent_choice = ""
+choice = ""
+choice = "None"
+opponent_choice = ""
+radio.set_group(1)
+
+def on_forever():
+    check_winner()
+basic.forever(on_forever)
---
+++ main.ts
@@ -0,0 +1,43 @@
+function check_winner () {
+    if (choice == opponent_choice) {
+        basic.showString("Tie")
+    } else if (choice == "Rock") {
+        if (opponent_choice == "Paper") {
+            basic.showLeds(`
+                # . . . .
+                # . . . .
+                # . . . .
+                # . . . .
+                # # # # .
+                `)
+        }
+    } else {
+    	
+    }
+}
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Square)
+    choice = "Rock"
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.No)
+    choice = "Scissors"
+})
+radio.onReceivedString(function (receivedString) {
+    opponent_choice = receivedString
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Chessboard)
+    choice = "Paper"
+})
+input.onGesture(Gesture.Shake, function () {
+    radio.sendString(choice)
+})
+let opponent_choice = ""
+let choice = ""
+choice = "None"
+opponent_choice = ""
+radio.setGroup(1)
+basic.forever(function () {
+    check_winner()
+})
